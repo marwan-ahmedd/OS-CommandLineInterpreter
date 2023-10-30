2423ac15c2d44806ec3b18db9552edd45454e4ba
Marwan <marawan1132003@gmail.com>
2023-10-30 20:39:10 +0200

    finished first 3 functions

--- Terminal.java
+++ Terminal.java
@@ -1,21 +1,47 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
-
 public class Terminal {
     Parser parser;
-    Path currentPath;
+    Path currentPath, homeDir;
     public Terminal() {
-        currentPath = new File(System.getProperty("user.home")).toPath();
+        homeDir = new File(System.getProperty("user.home")).toPath();
+        currentPath = homeDir;
         parser = new Parser();
     }
 
-    public String pwd() {
-        return "";
+    public void echo(String arg) {
+        System.out.println(arg);
     }
-    public void cd(String[] args) {}
+
+    public String pwd() {
+        return currentPath.toString();
+    }
+    public void cd() {
+        currentPath = homeDir;
+        System.out.println(currentPath);;
+    }
+    public void cd(String path) {
+        if (path.equals("..")) {
+            currentPath = currentPath.getParent();
+            System.out.println(currentPath);;
+        } else {
+            try {
+                currentPath = currentPath.resolve(path).toRealPath(LinkOption.NOFOLLOW_LINKS);
+                System.out.println(currentPath);;
+            } catch (IOException e) {
+                System.out.println("Invalid Path");
+            }
+        }
+    }
 
     public void chooseCommandAction() {}
     public static void main(String[] args) {
         Terminal terminal = new Terminal();
+        System.out.println(terminal.pwd());
+        terminal.cd("../test");
+        terminal.cd("..");
+
     }
 }
