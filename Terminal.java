dbd7f8933e309a89ea706d15afd80191a05cab1b
Marwan <marawan1132003@gmail.com>
2023-10-30 18:42:30 +0200

    init terminal class

--- Terminal.java
+++ Terminal.java
@@ -1,16 +1,21 @@
+import java.io.File;
 import java.nio.file.Path;
 
 public class Terminal {
     Parser parser;
     Path currentPath;
+    public Terminal() {
+        currentPath = new File(System.getProperty("user.home")).toPath();
+        parser = new Parser();
+    }
 
-    public String pwd() { return ""; }
+    public String pwd() {
+        return "";
+    }
     public void cd(String[] args) {}
 
-    public void chooseCommandAction() {
-
-    }
+    public void chooseCommandAction() {}
     public static void main(String[] args) {
-
+        Terminal terminal = new Terminal();
     }
 }
